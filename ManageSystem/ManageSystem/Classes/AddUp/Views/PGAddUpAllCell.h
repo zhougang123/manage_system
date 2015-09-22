3982d5035b75912185a0ebebb0308a327751137f
zhougang <zhougang@qfpay.com>
2015-09-22 20:59:11 +0800

    defines

---
+++ ManageSystem/ManageSystem/Classes/AddUp/Views/PGAddUpAllCell.h
@@ -0,0 +1,15 @@
+//
+//  PGAddUpAllCell.h
+//  ManageSystem
+//
+//  Created by mac on 15/9/22.
+//  Copyright (c) 2015年 qfpay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#define PGAddUpAllCellHeight 
+
+@interface PGAddUpAllCell : UITableViewCell
+
+@end
